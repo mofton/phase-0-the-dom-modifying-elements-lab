9a8f3fbc8825ddd6e6163581390e332a663d6f40
Mofeid Moshreky <mof_ton@yahoo.com>
2023-03-17 01:34:59 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Bob is the champion";
+document.body.append(newHeader);
+// Code below is follow along/experimental.
+const element = document.createElement("div");
+document.body.append(element);
+const ul = document.createElement("ul");
+for (let i = 0; i < 3; i += 1) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+element.append(ul);
+const element2 = document.getElementById("main");
+element2.style.height = "300px";
+element2.style.backgroundColor = "#27647B";
+element2.textContent = "You've changed what's on the screen!";
+element2.style.fontSize = "24px";
+element2.style.marginLeft = "30px";
+element2.style.lineHeight = 2;
+// element.remove();
